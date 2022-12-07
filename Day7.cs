10c33226684ab70bb3b32828675ab847f1e3b722
Robert Horvath <robert.horvath@mails.ch>
2022-12-07 08:45:11 +0000

    Update settings

---
+++ Day7.cs
@@ -0,0 +1,23 @@
+namespace AOC2022
+{
+    public class Day7 : Day
+    {
+        public void Part1()
+        {
+            StartExec();
+
+            var inputLines = LoadInputFile("./puzzles/Day7.txt");
+
+            var sum = 0;
+
+            foreach(string line in inputLines)
+            {
+                // ToDo
+            }
+
+            Console.WriteLine($":: Final Score = {sum}");
+
+            StopExec();
+        }
+    }
+}
