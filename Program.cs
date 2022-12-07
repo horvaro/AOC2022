36ac792ac60db51edd881f0f718c55ea100ed7e4
Robert Horvath <robert.horvath@mails.ch>
2022-12-07 11:54:03 +0000

    Restore Terminal Selection

--- Program.cs
+++ Program.cs
@@ -6,10 +6,10 @@
         {
             Console.WriteLine(":::::: AOC 2022 ::::::");
             Console.WriteLine(":::: Which Day do you want to run? ");
-            //var consoleAnswer = Console.ReadLine();
+            var consoleAnswer = Console.ReadLine();
             
             //DEBUG
-            var consoleAnswer = "7";
+            //var consoleAnswer = "7";
 
             consoleAnswer = consoleAnswer?.Trim();
 
