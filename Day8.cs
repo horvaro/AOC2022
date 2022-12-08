5056d3af519a8bf12a4b997810aa4d4b0dc9f109
Robert Horvath <robert.horvath@mails.ch>
2022-12-08 09:25:29 +0000

    Day8 Part2

--- Day8.cs
+++ Day8.cs
@@ -10,10 +10,7 @@ namespace AOC2022
 
             var visibleTrees = 0;
 
-            // Create 2D Array, which is as wide as a sinlge line in inputLines
-            // and as long as the whole file
             var forestMap = new int[inputLines.Count(),inputLines[0].Length];
-
             for (int i=0; i < forestMap.GetLength(0); i++)
             {
                 var forestSlice = inputLines[i];
@@ -32,27 +29,21 @@ namespace AOC2022
             {
                 for (int j=1; j < (forestMap.GetLength(1) -1); j++)
                 {
-                    IsTreeVisible = false;
-
-                    //From Top
                     var fromTopInvis = Enumerable.Range(0,i)
                                                 .Select(x => forestMap[x,j])
                                                 .Where(x => x >= forestMap[i,j])
                                                 .Any();
 
-                    //From Bottom
                     var fromBottomInvis = Enumerable.Range(i+1, forestMap.GetLength(0)-(i+1))
                                                 .Select(x => forestMap[x,j])
                                                 .Where(x => x >= forestMap[i,j])
                                                 .Any();
 
-                    //From Left
                     var fromLeftInvis = Enumerable.Range(0, j)
                                                 .Select(x => forestMap[i,x])
                                                 .Where(x => x >= forestMap[i,j])
                                                 .Any();
 
-                    //From Right
                     var fromRightInvis = Enumerable.Range(j+1, forestMap.GetLength(1)-(j+1))
                                                 .Select(x => forestMap[i,x])
                                                 .Where(x => x >= forestMap[i,j])
@@ -60,15 +51,93 @@ namespace AOC2022
 
                     IsTreeVisible = !fromTopInvis || !fromBottomInvis || !fromLeftInvis || !fromRightInvis;
 
-                    //Console.WriteLine($"Tree at [{i}][{j}] (={forestMap[i,j]}) visible = {IsTreeVisible}");
-
                     if (IsTreeVisible) visibleTrees++;
                 }
             }
 
             Console.WriteLine($":: Visible Trees = {visibleTrees}");
+            StopExec();
+
+            Part2(forestMap);
+        }
+
+        private void Part2 (int[,] forestMap)
+        {
+            StartExec();
+            Console.WriteLine("::: Part 2");
+
+            var maxScenicScore = 0;
+
+            for (int i=0; i < forestMap.GetLength(0); i++)
+            {
+                for (int j=0; j < forestMap.GetLength(1); j++)
+                {
+                    var up = GetViewDistUpwards(i,j,forestMap);
+                    var down = GetViewDistDownwards(i,j,forestMap);
+                    var left = GetViewDistLeft(i,j,forestMap);
+                    var right = GetViewDistRight(i,j,forestMap);
+                    var scenicScore = up * down * left * right;
+
+                    maxScenicScore = scenicScore > maxScenicScore ? scenicScore : maxScenicScore;
+                }
+            }
+
+            Console.WriteLine($":: highest scenic score possible = {maxScenicScore}");
 
             StopExec();
         }
+
+        private int GetViewDistUpwards(int column, int row, int[,] map)
+        {
+            if (column==0) return 0;
+            var tree = map[column,row];
+            var viewDist = 0;
+            for (int i=(column-1); i>=0; i--)
+            {
+                viewDist++;
+                if (map[i,row] >= tree) break;
+            }
+            return viewDist;
+        }
+
+        private int GetViewDistDownwards(int column, int row, int[,] map)
+        {
+            if (column==(map.GetLength(0)-1)) return 0;
+            var tree = map[column,row];
+            var viewDist = 0;
+            for (int i=(column+1); i<map.GetLength(0); i++)
+            {
+                viewDist++;
+                if (map[i,row] >= tree) break;
+            }
+            return viewDist;
+        }
+
+        private int GetViewDistLeft(int column, int row, int[,] map)
+        {
+            if (row==0) return 0;
+            var tree = map[column,row];
+            var viewDist = 0;
+            for (int i=(row-1); i>=0; i--)
+            {
+                viewDist++;
+                if (map[column,i] >= tree) break;
+            }
+            return viewDist;
+        }
+
+        private int GetViewDistRight(int column, int row, int[,] map)
+        {
+            if (row==(map.GetLength(1)-1)) return 0;
+            var tree = map[column,row];
+            var viewDist = 0;
+            for (int i=(row+1); i<map.GetLength(1); i++)
+            {
+                viewDist++;
+                if (map[column,i] >= tree) break;
+            }
+            return viewDist;
+        }
+
     }
 }
