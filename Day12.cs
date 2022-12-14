b9592660715c72426012ee5d1de9e71b3e3475ed
Robert Horvath <robert.horvath@mails.ch>
2022-12-14 17:31:30 +0000

    Day12, Create Map from Input

--- Day12.cs
+++ Day12.cs
@@ -1,3 +1,5 @@
+using System.Numerics;
+
 namespace AOC2022
 {
     public abstract class Day12 : Day
@@ -6,18 +8,47 @@ namespace AOC2022
         {
             StartExec();
 
-            var inputLines = LoadInputFile("./puzzles/Day12.txt");
+            var inputLines = LoadInputFile("./puzzles/Day12Example.txt");
 
             var sum = 0;
+            var start = new Vector2();
+            var end = new Vector2();
 
+            var map = new int[inputLines.Count(),inputLines.First().Length];
+            var n = 0;
             foreach (string line in inputLines)
             {
-                // ToDo
+                var elevations = line.ToCharArray();
+                for (int i = 0; i < line.Length; i++)
+                {
+                    var elevation = elevations[i];
+                    map[n,i] = elevation;
+                    if (elevation.Equals('S')) { start.X = n; start.Y = i; }
+                    if (elevation.Equals('E')) { end.X = n; end.Y = i; }
+                }
+                n++;
             }
+
+            PrintMap(map);
+
+
 
             Console.WriteLine($":: Final Score = {sum}");
 
             StopExec();
+        }
+
+        private static void PrintMap(int[,] map)
+        {
+            for (int i = 0; i < map.GetLength(0); i++)
+            {
+                for (int j = 0; j < map.GetLength(1); j++)
+                {
+                    Console.Write((char)map[i,j]);
+                }
+                Console.Write(Environment.NewLine);
+            }
+            Console.Write(Environment.NewLine);
         }
     }
 }
