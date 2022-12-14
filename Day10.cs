4b3ff9b31ac109ff0b55487b5c958dc6161f6d7c
Robert Horvath <robert.horvath@mails.ch>
2022-12-14 05:48:11 +0000

    Day10

--- Day10.cs
+++ Day10.cs
@@ -8,16 +8,113 @@ namespace AOC2022
 
             var inputLines = LoadInputFile("./puzzles/Day10.txt");
 
-            var sum = 0;
+            var targetCycles = new List<int>() { 20, 60, 100, 140, 180, 220 };
+            var signalStrengthSum = 0;
+
+            var regX = 1;
+            var cycle = 1;
+            var instructionCycles = 1;
 
             foreach (string line in inputLines)
             {
-                // ToDo
+                var instruction = line.Split(' ');
+
+                switch (instruction[0])
+                {
+                    case "noop":
+                        instructionCycles = 1;
+                        break;
+                    case "addx":
+                        instructionCycles = 2;
+                        break;
+                    default:
+                        break;
+                }
+
+                for (var i = 0; i < instructionCycles; i++)
+                {
+                    if (targetCycles.Contains(cycle))
+                    {
+                        var signalStrength = regX * cycle;
+                        signalStrengthSum += signalStrength;
+                    }
+
+                    cycle++;
+                }
+
+                switch (instruction[0])
+                    {
+                        case "addx":
+                            regX += Convert.ToInt32(instruction[1]);
+                            break;
+                        default:
+                            break;
+                    }
             }
 
-            Console.WriteLine($":: Final Score = {sum}");
+            Console.WriteLine($":: Final Score = {signalStrengthSum}");
 
             StopExec();
+
+            Part2(inputLines);
+        }
+
+        private static void Part2(string[] inputLines)
+        {
+            StartExec();
+            Console.WriteLine("::: Part 2");
+
+            var regX = 1;
+            var cycle = 0;
+            var instructionCycles = 1;
+
+            foreach (string line in inputLines)
+            {
+                var instruction = line.Split(' ');
+
+                switch (instruction[0])
+                {
+                    case "noop":
+                        instructionCycles = 1;
+                        break;
+                    case "addx":
+                        instructionCycles = 2;
+                        break;
+                    default:
+                        break;
+                }
+
+                for (var i = 0; i < instructionCycles; i++)
+                {
+                    Draw(cycle, regX);
+
+                    cycle++;
+                }
+
+                switch (instruction[0])
+                {
+                    case "addx":
+                        regX += Convert.ToInt32(instruction[1]);
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            Console.Write(Environment.NewLine);
+
+            StopExec();
+        }
+
+        private static void Draw (int cycle, int regX)
+        {
+            var crtRow = (cycle % 40);
+            if (crtRow == 0)
+            {
+                Console.Write(Environment.NewLine);
+            }
+            var pixel = (crtRow >= regX-1 && crtRow <= regX+1) ? "#" : ".";
+            Console.Write(pixel);
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -6,10 +6,10 @@
         {
             Console.WriteLine(":::::: AOC 2022 ::::::");
             Console.WriteLine(":::: Which Day do you want to run? ");
-            var consoleAnswer = Console.ReadLine();
+            //var consoleAnswer = Console.ReadLine();
 
             //DEBUG
-            //var consoleAnswer = "9";
+            var consoleAnswer = "10";
 
             consoleAnswer = consoleAnswer?.Trim();
 
