ed740901124e67c484db9808c6b58804b6a85a26
Robert Horvath <robert.horvath@mails.ch>
2022-12-19 17:02:52 +0000

    Load data Day 16

--- Day16.cs
+++ Day16.cs
@@ -10,14 +10,29 @@ namespace AOC2022
 
             var inputLines = LoadInputFile("./puzzles/Day16Example.txt");
             var lineRegex = new Regex(
-                @"^Sensor at x=(-?\d+), y=(-?\d+): closest beacon is at x=(-?\d+), y=(-?\d+)$",
+                @"^Valve (\S{2}) has flow rate=(-?\d+); tunnel(s?) lead(s?) to valve(s?) ((\S{2}(, )?)+)",
                 RegexOptions.Compiled  // Will be compiled at init, for exec performance
             );
             var sum = 0;
 
+            var flowRateLUT = new Dictionary<string,int>();
+            var valveConnections = new Dictionary<string,IList<string>>();
+
             foreach (string line in inputLines)
             {
-                // ToDo
+                var regexMatch = lineRegex.Match(line);
+                var valve = regexMatch.Groups[1].Value;
+                var flow = Convert.ToInt32(regexMatch.Groups[2].Value);
+                var connectedValves = regexMatch.Groups[6].Value.Split(',').AsEnumerable()
+                                                .Select(v => v.Trim()).ToList();
+                flowRateLUT.Add(valve, flow);
+                valveConnections.Add(valve, connectedValves);
+            }
+
+            var closedValves = new HashSet<string>();
+            for(var time = 30; time > 0; time--)
+            {
+
             }
 
             Console.WriteLine($":: Final Score = {sum}");
